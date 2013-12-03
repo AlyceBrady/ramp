11e01f7762dab237b1752269ac56bdcdfd628037
Alyce Brady <abrady@kzoo.edu>
2013-12-02 22:05:40 -0500

    Fix typo in function name in TableController

--- application/controllers/TableController.php
+++ application/controllers/TableController.php
@@ -264,7 +264,7 @@ class TableController extends Zend_Controller_Action
      */
     public function tableViewAction()
     {
-        $setTable = $this->_tblViewingSeq->getSetTableForTablularView();
+        $setTable = $this->_tblViewingSeq->getSetTableForTabularView();
 
         // Is this the initial display or a callback from a button action?
         //    (Or an illegal callback that could allow password corruption?)
